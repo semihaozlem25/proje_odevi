98f3cfbe50390fca85d368ed829423a830c1e523
Semiha Özlem Yılmaz <74215861+semihaozlem25@users.noreply.github.com>
2021-04-23 01:07:58 +0300

    intial commit

---
+++ app/src/main/java/com/example/proje_odevi/veriTabani.java
@@ -0,0 +1,14 @@
+package com.example.proje_odevi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class veriTabani extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_veri_tabani);
+    }
+}
