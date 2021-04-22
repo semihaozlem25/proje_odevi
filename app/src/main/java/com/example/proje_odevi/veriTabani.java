c12f23761ed85f9090367cd006a530fc6e937b54
Semiha Özlem Yılmaz <74215861+semihaozlem25@users.noreply.github.com>
2021-04-23 01:35:52 +0300

    intial commit

--- app/src/main/java/com/example/proje_odevi/veriTabani.java
+++ app/src/main/java/com/example/proje_odevi/veriTabani.java
@@ -1,14 +1,21 @@
 package com.example.proje_odevi;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 
-public class veriTabani extends AppCompatActivity {
+public class veriTabani extends SQLiteOpenHelper {
+    public veriTabani( Context context) {
+        super(context, "veriTabaniAdi", null, 1);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_veri_tabani);
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 }
