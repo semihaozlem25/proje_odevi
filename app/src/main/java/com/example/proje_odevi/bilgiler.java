a6188d7de91c1f3bf9e5d6f686b291f88c3f5353
Semiha Özlem Yılmaz <74215861+semihaozlem25@users.noreply.github.com>
2021-06-11 15:13:30 +0300

    intial commit

---
+++ app/src/main/java/com/example/proje_odevi/bilgiler.java
@@ -0,0 +1,14 @@
+package com.example.proje_odevi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bilgiler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bilgiler);
+    }
+}
